6dcc176cd8e14cc525aa643f3da831162f77ac65
Joel Ljunggren <joelljunggren@hotmail.com>
2024-02-20 10:04:50 +0100

    first commit

---
+++ src/app/register/page.tsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+
+const YourComponent: React.FC = () => {
+  const productivityRating = [1, 2, 3, 4, 5, 6, 7];
+  const stressRating = [1, 2, 3, 4, 5, 6, 7];
+
+  return (
+    <>
+    {/* Måste göra om det här till komponenter */}
+      <div className="formDiv">
+        <div className="formGreeting">Redigstrera dina nuvarande nivåer</div>
+        <form>
+          <div className="textLabel">Fyll i produktivitetsnivå</div>
+          {productivityRating.map((productivityRating) => (
+            <label key={productivityRating}>
+              <input type="radio" name="rating" value={productivityRating} />
+              {productivityRating}
+            </label>
+          ))}
+            <div className="textLabel">Fyll i stressnivå</div>
+            {stressRating.map((stressRating) => (
+            <label key={stressRating}>
+              <input type="radio" name="rating" value={stressRating} />
+              {stressRating}
+            </label>
+          ))}
+            <div className="textLabel">Meddelande</div>
+          <textarea className="inputField" />
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default function Page() {
+  return <YourComponent />;
+}
