7c279b3d5d4e81c93dad24bd78d5751f9e45c1e0
Joel Ljunggren <joelljunggren@hotmail.com>
2024-04-17 09:03:19 +0200

    Added placeholder monthly view

--- src/app/monthlyview/page.tsx
+++ src/app/monthlyview/page.tsx
@@ -10,8 +10,78 @@ const Page: React.FC = () => {
     <>
     <Header/>
     <div className="monthlyDiv">
-    <h1>Månadsöversikt</h1>
         <div className="monthlyAverages">
+            <div className="month">Januari
+            </div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Februari</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Mars</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">April</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Maj</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Juni</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Juli</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Augusti</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">September</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">Oktober</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">November</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
+        </div>
+        <div className="monthlyAverages">
+            <div className="month">December</div>
+            <div className="monthlyStressAndProductivity">Stress: 5</div>
+            <div className="monthlyStressAndProductivity">Produktivitet: 5</div>
+            <div className="monthlyEntries">12 inlägg</div>
         </div>
     </div>
     <Footer/>
